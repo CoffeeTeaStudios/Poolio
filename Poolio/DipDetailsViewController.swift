705f55897301578c7e808b1b3aa0f2dad9b1a594
Kelvin Lau <kelvinlau@Kelvins-iMac.local>
2016-09-25 04:08:27 -0700

    more UI

--- Poolio/DipDetailsViewController.swift
+++ Poolio/DipDetailsViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 final class DipDetailsViewController: UIViewController {
+  @IBOutlet private var tableView: UITableView!
   
+}
+
+// MARK: - UITableViewDataSource
+extension DipDetailsViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: PoolCell.reuseIdentifier) as? PoolCell else { fatalError() }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension DipDetailsViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    performSegue(withIdentifier: .poolDetail, sender: nil)
+  }
 }
 
 // MARK: - Life Cycle Methods
@@ -20,5 +41,28 @@ extension DipDetailsViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+}
+
+// MARK: - SegueHandlerType
+extension DipDetailsViewController: SegueHandlerType {
+  enum SegueIdentifier: String {
+    case contribute, poolDetail
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    switch segueIdentifierFor(segue: segue) {
+    case .contribute:
+      break
+    case .poolDetail:
+      break
+    }
+  }
+}
+
+// MARK: - @IBActions
+private extension DipDetailsViewController {
+  @IBAction func dipButtonTapped() {
+    performSegue(withIdentifier: .contribute, sender: nil)
   }
 }
---
+++ Poolio/PoolDetailsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PoolDetailsViewController.swift
+//  Poolio
+//
+//  Created by Kelvin Lau on 2016-09-25.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+final class PoolDetailsViewController: UIViewController {
+  @IBOutlet fileprivate var teamImageView: UIImageView! { didSet {
+    teamImageView.image = UIImage.imageWith(color: .groupTableViewBackground)
+  }}
+  
+  @IBOutlet fileprivate var chanceImageView: UIImageView! { didSet {
+    chanceImageView.image = UIImage.imageWith(color: UIColor(red: 255/255, green: 209/255, blue: 76/255, alpha: 1))
+  }}
+}
+
+// MARK: - Life Cycle Methods
+extension PoolDetailsViewController {
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    tabBarController?.tabBar.isHidden = true
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    
+    teamImageView.layer.cornerRadius = teamImageView.frame.height / 2
+    chanceImageView.layer.cornerRadius = chanceImageView.frame.height / 2
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    tabBarController?.tabBar.isHidden = false
+  }
+}
+
+// MARK: - SegueHandlerType
+extension PoolDetailsViewController: SegueHandlerType {
+  enum SegueIdentifier: String {
+    case groupContribution
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    switch segueIdentifierFor(segue: segue) {
+    case .groupContribution:
+      break
+    }
+  }
+}
+
+// MARK: - @IBActions
+private extension PoolDetailsViewController {
+  @IBAction func requestButtonTapped() {
+    performSegue(withIdentifier: .groupContribution, sender: nil)
+  }
+}
