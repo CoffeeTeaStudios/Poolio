a5cc3d4f64300d258ed5993f9aede19d2f6cf769
Kelvin Lau <kelvinlau@Kelvins-iMac.local>
2016-09-25 00:19:51 -0700

    Added more things.

---
+++ Poolio/DipViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DipViewController.swift
+//  Poolio
+//
+//  Created by Kelvin Lau on 2016-09-25.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+final class DipViewController: UIViewController {
+  
+}
+
+// MARK: - Life Cycle Methods
+extension DipViewController {
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    self.navigationController?.isNavigationBarHidden = true
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    self.navigationController?.isNavigationBarHidden = false
+  }
+}
+
+extension DipViewController: SegueHandlerType {
+  enum SegueIdentifier: String {
+    case dipDetails
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    switch segueIdentifierFor(segue: segue) {
+    case .dipDetails:
+      break
+    }
+  }
+}
+
+// MARK: - @IBActions
+private extension DipViewController {
+  @IBAction func joinButtonTapped() {
+    performSegue(withIdentifier: .dipDetails, sender: nil)
+  }
+}
---
+++ Poolio/Theme.swift
@@ -0,0 +1,9 @@
+//
+//  Theme.swift
+//  Poolio
+//
+//  Created by Kelvin Lau on 2016-09-25.
+//  Copyright © 2016 Kelvin. All rights reserved.
+//
+
+import Foundation
